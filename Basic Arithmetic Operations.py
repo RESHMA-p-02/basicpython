46d6fc590e1892a40c075c4476b32396259b195b
RESHMA-p-02 <166393122+RESHMA-p-02@users.noreply.github.com>
2024-04-08 17:11:51 +0530

    Add files via upload
    teached  python

---
+++ Basic Arithmetic Operations.py
@@ -0,0 +1,14 @@
+# This program performs basic arithmetic operations on variables
+# Assign values to variables
+num1 = 10
+num2 = 5
+# Perform addition, subtraction, multiplication, and division
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+# Print the results
+print("Sum:", sum)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
