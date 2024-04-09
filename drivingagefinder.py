9685f89ea035a0d47d96d132bc0cad56258739fd
RESHMA-p-02 <166393122+RESHMA-p-02@users.noreply.github.com>
2024-04-09 15:31:26 +0530

    Add files via upload
    I made a driving age finder

---
+++ drivingagefinder.py
@@ -0,0 +1,7 @@
+age = int(input("enter your age"))
+driving_age=18
+can_drive=age>=driving_age
+if can_drive:
+    print("your are old enough to drive")
+else:
+    print("your are not old  to drive")
